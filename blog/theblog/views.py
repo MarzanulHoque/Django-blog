db8b77af0c2d1d3a82f85dba43a9d7670d62f24b
marzanulruhoque <marzanulru17@gmail.com>
2021-08-05 17:13:45 -0700

    List view and DetailView added For post

--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
+from .models import Post
 
 # Create your views here.
 
-def home(request):
-    return render(request,'theblog/home.html',{})
+class HomeView(ListView):
+
+    model = Post
+    template_name="theblog/home.html"
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name="theblog/detail.html"
