1de0fbd73f7e2b6441b15e0f446b9a821a15ef4c
marzanulruhoque <marzanulru17@gmail.com>
2021-08-05 22:42:17 -0700

    CreateView Added For New Post

--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
 from .models import Post
 
 # Create your views here.
@@ -12,3 +12,9 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name="theblog/detail.html"
+
+class AddPostView(CreateView):
+    model = Post
+    template_name="theblog/add_post.html"
+
+    fields='__all__'
