8462a7f3072be57e9c0b3a84788c0a29753f035a
marzanulruhoque <marzanulru17@gmail.com>
2021-08-16 20:03:42 -0700

    fullname and emailfield added to registration

--- blog/members/views.py
+++ blog/members/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SignUpForm
+
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
