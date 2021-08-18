874d3433fd483ad6310ccd822774f5b333a5a4f3
marzanulruhoque <marzanulru17@gmail.com>
2021-08-17 20:55:01 -0700

    Post Snippetfield added to model

--- blog/theblog/models.py
+++ blog/theblog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
     body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=200, default="Uncategorized")
+    snippet = models.CharField(max_length=200)
     likes = models.ManyToManyField(User, related_name='blog_posts')
 
     def total_likes(self):
