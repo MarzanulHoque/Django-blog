dc1dacf4fc6e790f64ff3f3b5b1081876d5bab51
marzanulruhoque <marzanulru17@gmail.com>
2021-08-17 21:10:59 -0700

    Edit Profile Page Added

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, UserChangeView
 
 urlpatterns = [
-    path('register/',RegisterView.as_view(),name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('edit_profile/', UserChangeView.as_view(), name='edit_profile'),
 ]
--- blog/members/views.py
+++ blog/members/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 
@@ -7,3 +8,12 @@ class RegisterView(CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserChangeView(UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
